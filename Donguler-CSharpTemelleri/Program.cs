6911d3d54020fc3b56e92bfcc6844b8f2e85ef62
= <=>
2022-06-13 15:38:52 +0300

    first-upload-2

--- Donguler-CSharpTemelleri/Program.cs
+++ Donguler-CSharpTemelleri/Program.cs
@@ -1,1 +1,7 @@
 ﻿Console.WriteLine("Hello CSharp Temelleri - Döngüler");
+
+
+// Döngüler 
+
+// Neden döngüleri kullanırız ?
+// Döngüleri birbirine benzeyen işlemleri tekrar etmek için kullanırız.
